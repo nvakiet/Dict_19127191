84c83911a421b3d1a4ead64e2ccf53d564e4d822
nvakiet <63222542+nvakiet@users.noreply.github.com>
2022-01-05 06:33:59 +0700

    Create artifact: Program JAR.

--- src/vn/edu/hcmus/student/sv19127191/Main.java
+++ src/vn/edu/hcmus/student/sv19127191/Main.java
@@ -1,13 +1,6 @@
 package vn.edu.hcmus.student.sv19127191;
 
-import vn.edu.hcmus.student.sv19127191.dict.Dictionary;
 import vn.edu.hcmus.student.sv19127191.ui.MainFrame;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
@@ -17,28 +10,10 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
  * Description: ...<br/>
  */
 public class Main {
-	public static void mainTest(String[] args) {
-		try {
-			Dictionary dict = new Dictionary();
-			double[] measures = new double[10];
-			for (int i = 0; i < 10; ++i) {
-				long startTime = System.nanoTime();
-				dict.load("data/init/original.dat");
-				long stopTime = System.nanoTime();
-				measures[i] = (stopTime - startTime) / 1_000_000_000.0;
-				System.out.printf("Time elapsed: %.6f\n", measures[i]);
-			}
-			System.out.printf("Average time: %.6f\n", Arrays.stream(measures).average().orElse(Double.NaN));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public static void main(String[] args) {
 		MainFrame frame = new MainFrame();
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
-		//mainTest(null);
 	}
 }
