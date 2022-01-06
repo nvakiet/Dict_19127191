e1f6f4eef2069afa002c33c2224eccb8311d9639
nvakiet <63222542+nvakiet@users.noreply.github.com>
2022-01-06 22:55:06 +0700

    Add Javadoc comments to classes: History and Record. Also build a new version of artifact.

--- src/vn/edu/hcmus/student/sv19127191/dict/History.java
+++ src/vn/edu/hcmus/student/sv19127191/dict/History.java
@@ -20,10 +20,16 @@ public class History {
 	private boolean shouldRefresh = false;
 	public boolean shouldSave = false;
 
+	/**
+	 * The default constructor of this class
+	 */
 	public History() {
 		queryRecords = new ArrayList<>();
 	}
 
+	/**
+	 * Save the query history to a file on the hard drive (data/saved/history.dat)
+	 */
 	public void save() throws Exception {
 		if (!shouldSave) return;
 		File file = new File("data/saved/history.dat");
@@ -33,6 +39,10 @@ public class History {
 		shouldSave = false;
 	}
 
+	/**
+	 * Load the query history from a file on the hard drive (data/saved/history.dat)
+	 * @throws Exception
+	 */
 	@SuppressWarnings("unchecked")
 	public void load() throws Exception {
 		File file = new File("data/saved/history.dat");
@@ -44,12 +54,20 @@ public class History {
 		}
 	}
 
+	/**
+	 * Add a query record to this history.
+	 * @param rec The record of a dictionary search query.
+	 */
 	public void addRecord(Record rec) {
 		queryRecords.add(rec);
 		shouldSave = true;
 		shouldRefresh = true;
 	}
 
+	/**
+	 * Get the whole history as a 2d matrix: each row has 3 columns: query date, query string, query type.
+	 * @return The history table
+	 */
 	public String[][] getTable() {
 		if (table == null || shouldRefresh) {
 			table = new String[queryRecords.size()][3];
@@ -62,18 +80,33 @@ public class History {
 	}
 }
 
+/**
+ * This class is a wrapper class for the history records.<br>
+ * It holds record for: query date, query string and query type.<br>
+ * This is serializable to save the record in a file.
+ */
 class Record implements Serializable {
 	public LocalDateTime time;
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	public String query;
 	public boolean isSlang;
 
+	/**
+	 * The primary constructor of this class.
+	 * @param time The date and time of when the query is performed.
+	 * @param query The query string.
+	 * @param isSlang The type of the query: slang or definition.
+	 */
 	public Record(LocalDateTime time, String query, boolean isSlang) {
 		this.time = time;
 		this.query = query;
 		this.isSlang = isSlang;
 	}
 
+	/**
+	 * Convert this record to a row in the history table.
+	 * @return The table row as an array of 3 strings: date time, query string, query type
+	 */
 	public String[] toTableRow() {
 		String[] row = new String[3];
 		row[0] = time.format(formatter);
@@ -82,6 +115,11 @@ class Record implements Serializable {
 		return row;
 	}
 
+	/**
+	 * Convert a row in the history table into a Record.
+	 * @param row The table row with 3 string columns: date time, query string, query time
+	 * @return A new Record
+	 */
 	public static Record fromTableRow(String[] row) {
 		return new Record(
 				LocalDateTime.parse(row[0], Record.formatter),
